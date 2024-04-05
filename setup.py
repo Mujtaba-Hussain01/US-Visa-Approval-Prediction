ee4af01478b005e8dd49884a534a39f6db41acf4
Mohammed Mujtaba Hussain <mujtabamohammed.h123@gmail.com>
2024-04-06 00:15:11 +0530

    utils

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='us_visa',
+    version='0.0.0',
+    auther="Mujtaba",
+    author_email="mujtaba.7794@gmail.com",
+    packages=find_packages()
+)

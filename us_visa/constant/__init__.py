8cd0a690fcd88bcfb917f0c96c47c9c043c24612
Mohammed Mujtaba Hussain <mujtabamohammed.h123@gmail.com>
2024-04-14 09:43:58 +0530

    data transformation

--- us_visa/constant/__init__.py
+++ us_visa/constant/__init__.py
@@ -33,9 +33,19 @@ DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
 """
-Data Validation related constant start with DATA_VALIDATION var name
+Data Validation related constant start with DATA_VALIDATION VAR NAME
 """
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+"""
+Data Transformation related constant start with TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+
+
 
